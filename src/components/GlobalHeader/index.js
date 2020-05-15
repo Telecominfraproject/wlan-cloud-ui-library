ad17f5c6e6f5b1d2e6489aaed1489eb2a986ee61
Sean Macfarlane <sean.g.macfarlane@gmail.com>
2020-05-15 12:22:16 -0400

    fixed header

--- src/components/GlobalHeader/index.js
+++ src/components/GlobalHeader/index.js
@@ -1,56 +1,17 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Layout, Popover, Row } from 'antd';
-import { MenuUnfoldOutlined, MenuFoldOutlined, SettingOutlined } from '@ant-design/icons';
+import { Layout } from 'antd';
+import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
 
 import ThemeContext from 'contexts/ThemeContext';
+import SettingsDropdown from 'components/SettingsDropdown';
 import styles from './index.module.scss';
 
 const { Header } = Layout;
 
 const GlobalHeader = ({ collapsed, onMenuButtonClick, isMobile }) => {
   const theme = useContext(ThemeContext);
-  const [popoverVisible, setPopoverVisible] = useState(false);
-
-  const hidePopover = () => {
-    setPopoverVisible(false);
-  };
-
-  const handleVisibleChange = visible => {
-    setPopoverVisible(visible);
-  };
-
-  const userOptions = (
-    <>
-      <Row>
-        <Link onClick={hidePopover} to="/accounts/customers/view">
-          Profile
-        </Link>
-      </Row>
-      <Row>
-        <Link onClick={hidePopover} to="/account/edit">
-          Edit Account
-        </Link>
-      </Row>
-      <Row>
-        <Link onClick={hidePopover} to="/account">
-          Users
-        </Link>
-      </Row>
-      <Row>
-        <Link onClick={hidePopover} to="/accounts">
-          Advanced
-        </Link>
-      </Row>
-      <Row>
-        <Link onClick={hidePopover} to="/accounts/customersxw">
-          Rules Preference
-        </Link>
-      </Row>
-    </>
-  );
-
   return (
     <Header
       className={`${styles.GlobalHeader} ${collapsed ? styles.collapsed : ''} ${
@@ -68,17 +29,7 @@ const GlobalHeader = ({ collapsed, onMenuButtonClick, isMobile }) => {
         <MenuFoldOutlined className={styles.MenuIcon} onClick={onMenuButtonClick} />
       )}
       <div className={styles.RightMenu}>
-        <Popover
-          content={userOptions}
-          trigger="click"
-          getPopupContainer={e => e.parentElement}
-          visible={popoverVisible}
-          onVisibleChange={handleVisibleChange}
-          placement="bottomRight"
-          arrowPointAtCenter
-        >
-          <SettingOutlined className={styles.MenuIcon} />
-        </Popover>
+        <SettingsDropdown />
       </div>
     </Header>
   );
