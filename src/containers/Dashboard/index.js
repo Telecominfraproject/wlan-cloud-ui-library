98043a1b921f90630bfe39cd30ad4e2977f2d815
Ali Muhammad <ali.muhammad@connectus.ai>
2020-06-29 17:13:25 -0400

    added pie charts to dashboard screen

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import DeviceStatsCard from './components/DeviceStatsCard';
+import PieChart from './components/PieChart';
 import styles from './index.module.scss';
 
-const Dashboard = ({ titleList, statsArr }) => {
+const Dashboard = ({ titleList, statsArr, pieChartData }) => {
+  const titles = ['AP Vendors', 'Client Vendors'];
   return (
     <div className={styles.mainInfoWrap}>
       <div className={styles.rightInfoWrap}>
@@ -13,6 +15,11 @@ const Dashboard = ({ titleList, statsArr }) => {
               return <DeviceStatsCard title={titleList[index]} cardData={data} />;
             })}
         </div>
+        <div className={styles.pieChartWrap}>
+          {pieChartData.map((data, index) => {
+            return <PieChart chartData={data} title={titles[index]} />;
+          })}
+        </div>
       </div>
     </div>
   );
@@ -21,6 +28,7 @@ const Dashboard = ({ titleList, statsArr }) => {
 Dashboard.propTypes = {
   titleList: PropTypes.instanceOf(Array).isRequired,
   statsArr: PropTypes.instanceOf(Array).isRequired,
+  pieChartData: PropTypes.instanceOf(Array).isRequired,
 };
 
 export default Dashboard;
