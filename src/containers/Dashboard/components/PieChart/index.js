fbbce777fba713eca71b8eda9e517d7bb79e4378
Sean Macfarlane <sean.g.macfarlane@gmail.com>
2020-06-30 12:11:48 -0400

    fixed dashboard

--- src/containers/Dashboard/components/PieChart/index.js
+++ src/containers/Dashboard/components/PieChart/index.js
@@ -1,55 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'antd';
 import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 import styles from './index.module.scss';
 
+const headerStyle = {
+  textAlign: 'center',
+  border: 0,
+};
+
 const PieChart = ({ chartData, title }) => {
-  const [pieData, setPieData] = useState([]);
-  const headerStyle = {
-    textAlign: 'center',
-    border: 0,
-  };
-  const bodyStyle = {};
-  const chartId = title.replace(/\s/g, '');
-  const highChartsRender = () => {
-    Highcharts.chart({
-      chart: {
-        type: 'pie',
-        renderTo: chartId,
-      },
-      title: {
-        verticalAlign: 'middle',
-        floating: true,
-        text: '',
-        style: {
-          fontSize: '10px',
-        },
-      },
-      plotOptions: {
-        pie: {
-          dataLabels: {
-            color: '#fff',
-            textShadow: 'none',
-            enabled: true,
-            format: '{point.name}: {point.percentage:.1f} %',
-          },
-          innerSize: '0%',
-        },
-      },
-      credits: {
-        enabled: false,
-      },
-      series: pieData,
-    });
-  };
-
-  useEffect(() => {
-    highChartsRender();
-  }, [pieData]);
-
-  useEffect(() => {
+  const pieData = useMemo(() => {
     const values = Object.values(chartData);
     const pieDataArr = [
       {
@@ -65,12 +28,41 @@ const PieChart = ({ chartData, title }) => {
       };
       return pieDataArr[0].data.push(pieDataObj);
     });
-    setPieData(pieDataArr);
-  }, []);
+    return pieDataArr;
+  });
+
+  const options = {
+    chart: {
+      type: 'pie',
+    },
+    title: {
+      verticalAlign: 'middle',
+      floating: true,
+      text: '',
+      style: {
+        fontSize: '10px',
+      },
+    },
+    plotOptions: {
+      pie: {
+        dataLabels: {
+          color: '#fff',
+          textShadow: 'none',
+          enabled: true,
+          format: '{point.name}: {point.percentage:.1f} %',
+        },
+        innerSize: '0%',
+      },
+    },
+    credits: {
+      enabled: false,
+    },
+    series: pieData,
+  };
 
   return (
-    <Card title={title} headStyle={headerStyle} bodyStyle={bodyStyle} className={styles.pieChart}>
-      <div id={chartId} />
+    <Card title={title} headStyle={headerStyle} className={styles.pieChart}>
+      <HighchartsReact highcharts={Highcharts} options={options} />
     </Card>
   );
 };
