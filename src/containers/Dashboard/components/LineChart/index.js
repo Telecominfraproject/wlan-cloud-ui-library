593327cce4e7c9e898fb649583e17d3ab7ae9c2b
Sean Macfarlane <sean.g.macfarlane@gmail.com>
2020-07-23 15:39:24 -0400

    changed chart colors

--- src/containers/Dashboard/components/LineChart/index.js
+++ src/containers/Dashboard/components/LineChart/index.js
@@ -32,6 +32,17 @@ const LineChart = ({ title, data, options }) => {
           time={{
             useUTC: false,
           }}
+          colors={[
+            '#058DC7',
+            '#50B432',
+            '#ED561B',
+            '#DDDF00',
+            '#24CBE5',
+            '#64E572',
+            '#FF9655',
+            '#FFF263',
+            '#6AF9C4',
+          ]}
         >
           <Chart type="spline" zoomType="x" backgroundColor="#141414" />
           <XAxis
--- src/containers/Dashboard/components/PieChart/index.js
+++ src/containers/Dashboard/components/PieChart/index.js
@@ -23,7 +23,19 @@ const PieChart = ({ chartData, title }) => {
     <div className={styles.container}>
       <Card title={title} headStyle={headerStyle} className={styles.pieChart}>
         {pieData.length > 0 ? (
-          <HighchartsChart>
+          <HighchartsChart
+            colors={[
+              '#058DC7',
+              '#50B432',
+              '#ED561B',
+              '#DDDF00',
+              '#24CBE5',
+              '#64E572',
+              '#FF9655',
+              '#FFF263',
+              '#6AF9C4',
+            ]}
+          >
             <PieSeries
               name="Count"
               data={pieData}
