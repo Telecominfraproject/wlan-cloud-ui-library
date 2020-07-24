28c8fbd05a85d4641c3d83fa247c29647dbebf3c
Alidev123 <ali.muhammad@connectus.ai>
2020-07-24 14:14:36 -0400

    added refresh time indicator to line charts

--- src/containers/Dashboard/components/LineChart/index.js
+++ src/containers/Dashboard/components/LineChart/index.js
@@ -12,7 +12,9 @@ import {
 import { Card } from 'antd';
 import Highcharts from 'highcharts/highstock';
 import PropTypes from 'prop-types';
-import Timer from '../../../AccessPointDetails/components/OS/components/Timer';
+import Timer from 'containers/AccessPointDetails/components/OS/components/Timer';
+
+import { COLORS } from 'utils/charts';
 import styles from './index.module.scss';
 
 const dateTimeLabelFormats = {
@@ -41,17 +43,7 @@ const LineChart = ({ title, data, options }) => {
           time={{
             useUTC: false,
           }}
-          colors={[
-            '#058DC7',
-            '#50B432',
-            '#ED561B',
-            '#DDDF00',
-            '#24CBE5',
-            '#64E572',
-            '#FF9655',
-            '#FFF263',
-            '#6AF9C4',
-          ]}
+          colors={COLORS}
         >
           <Chart type="spline" zoomType="x" backgroundColor="#141414" />
           <XAxis
---
+++ src/utils/charts.js
@@ -0,0 +1,1 @@
+export const COLORS = ['#1890ff', '#2fc25b', '#13c2c2', '#facc14', '#f04864', '#8543e0', '#FF9655'];
