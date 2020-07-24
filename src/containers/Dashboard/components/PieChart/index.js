86d4ebeabf8e03c08a478618c1c4eb23b87c87a6
Sean Macfarlane <sean.g.macfarlane@gmail.com>
2020-07-24 11:38:11 -0400

    changed chart colors

--- src/containers/Dashboard/components/PieChart/index.js
+++ src/containers/Dashboard/components/PieChart/index.js
@@ -4,6 +4,7 @@ import { Card } from 'antd';
 import { HighchartsChart, withHighcharts, PieSeries, Tooltip } from 'react-jsx-highcharts';
 import Highcharts from 'highcharts/highstock';
 
+import { COLORS } from 'utils/charts';
 import styles from './index.module.scss';
 
 const headerStyle = {
@@ -23,19 +24,7 @@ const PieChart = ({ chartData, title }) => {
     <div className={styles.container}>
       <Card title={title} headStyle={headerStyle} className={styles.pieChart}>
         {pieData.length > 0 ? (
-          <HighchartsChart
-            colors={[
-              '#058DC7',
-              '#50B432',
-              '#ED561B',
-              '#DDDF00',
-              '#24CBE5',
-              '#64E572',
-              '#FF9655',
-              '#FFF263',
-              '#6AF9C4',
-            ]}
-          >
+          <HighchartsChart colors={COLORS}>
             <PieSeries
               name="Count"
               data={pieData}
