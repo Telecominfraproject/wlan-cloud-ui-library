4ceaf67079e8bc619b4a8aa3475a4791011d6cc4
Toan Do <toando@Toans-MacBook-Pro.local>
2020-09-17 12:45:22 -0400

    aligned fragment

--- src/containers/ProfileDetails/components/Radius/index.js
+++ src/containers/ProfileDetails/components/Radius/index.js
@@ -403,21 +403,21 @@ const RadiusForm = ({ form, details }) => {
                 extra={
                   <>
                     <>
-                          <b className={styles.iconButton}>{item.name}</b>
-                          <Button
-                            title="editRadiusServiceZone"
-                            onClick={() => handleEditZone(item)}
-                            className={styles.iconButton}
-                            icon={<EditOutlined />}
-                          />
-                          <Button
-                            title="deleteRadiusServiceZone"
-                            onClick={() => handleDeleteZone(item)}
-                            className={styles.iconButton}
-                            icon={<DeleteOutlined />}
-                            type="danger"
-                          />
-                        </>
+                      <b className={styles.iconButton}>{item.name}</b>
+                        <Button
+                          title="editRadiusServiceZone"
+                          onClick={() => handleEditZone(item)}
+                          className={styles.iconButton}
+                          icon={<EditOutlined />}
+                        />
+                        <Button
+                          title="deleteRadiusServiceZone"
+                          onClick={() => handleDeleteZone(item)}
+                          className={styles.iconButton}
+                          icon={<DeleteOutlined />}
+                          type="danger"
+                        />
+                    </>
                     <div className={styles.RadiusInline}>
                       <Card className={styles.infoCard} title=" Radius Services" bordered={false}>
                         <Table
