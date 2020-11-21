5fa7a20eb769dc8ba511050cfadb8e3c977d21bb
Sean Macfarlane <sean.g.macfarlane@gmail.com>
2020-11-21 00:49:28 -0500

    Added GenericNotFound

---
+++ src/components/GenericNotFound/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Result } from 'antd';
+
+const GenericNotFound = () => (
+  <Result status="404" title="404" subTitle="Sorry, the page you visited does not exist." />
+);
+
+export default GenericNotFound;
