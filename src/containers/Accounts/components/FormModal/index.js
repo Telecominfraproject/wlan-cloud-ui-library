872c64c3cd22feb6aa5ee4d33c65bbf478981372
chris-cosentino <chris.cosentino@netexperience.com>
2020-12-14 19:01:01 -0500

    remove password field if Auth0 enabled

--- src/containers/Accounts/components/FormModal/index.js
+++ src/containers/Accounts/components/FormModal/index.js
@@ -12,7 +12,7 @@ const strongRegex = new RegExp(
   /(?=.{8,})((?=.*\d)(?=.*[a-z])(?=.*[A-Z])|(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&*])|(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])).*/
 );
 
-const FormModal = ({ onCancel, onSubmit, visible, title, userEmail, userRole }) => {
+const FormModal = ({ onCancel, onSubmit, visible, title, userEmail, userRole, isAuth0Enabled }) => {
   const [form] = Form.useForm();
 
   useEffect(() => {
@@ -50,54 +50,57 @@ const FormModal = ({ onCancel, onSubmit, visible, title, userEmail, userRole }) 
           <Option value="CustomerIT">CustomerIT</Option>
         </Select>
       </Item>
+      {!isAuth0Enabled && (
+        <>
+          <Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password',
+              },
+              () => ({
+                validator(_rule, value) {
+                  if (!value || strongRegex.test(value)) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    new Error(
+                      'Password must be at least 8 characters in length and contain a Lower case letter, a Upper case letter, a Number, and a Special character.'
+                    )
+                  );
+                },
+              }),
+            ]}
+          >
+            <Input.Password visibilityToggle className={styles.field} />
+          </Item>
 
-      <Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password',
-          },
-          () => ({
-            validator(_rule, value) {
-              if (!value || strongRegex.test(value)) {
-                return Promise.resolve();
-              }
-              return Promise.reject(
-                new Error(
-                  'Password must be at least 8 characters in length and contain a Lower case letter, a Upper case letter, a Number, and a Special character.'
-                )
-              );
-            },
-          }),
-        ]}
-      >
-        <Input.Password visibilityToggle className={styles.field} />
-      </Item>
-
-      <Item
-        label="Confirm Password"
-        name="confirmedPassword"
-        dependencies={['password']}
-        rules={[
-          {
-            required: true,
-            message: 'Please confirm your password',
-          },
-          ({ getFieldValue }) => ({
-            validator(_rule, value) {
-              if (!value || getFieldValue('password') === value) {
-                return Promise.resolve();
-              }
-              return Promise.reject(new Error('The two passwords do not match'));
-            },
-          }),
-        ]}
-        hasFeedback
-      >
-        <Input.Password className={styles.field} />
-      </Item>
+          <Item
+            label="Confirm Password"
+            name="confirmedPassword"
+            dependencies={['password']}
+            rules={[
+              {
+                required: true,
+                message: 'Please confirm your password',
+              },
+              ({ getFieldValue }) => ({
+                validator(_rule, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error('The two passwords do not match'));
+                },
+              }),
+            ]}
+            hasFeedback
+          >
+            <Input.Password className={styles.field} />
+          </Item>
+        </>
+      )}
     </Form>
   );
 
@@ -129,6 +132,7 @@ FormModal.propTypes = {
   title: PropTypes.string,
   userEmail: PropTypes.string,
   userRole: PropTypes.string,
+  isAuth0Enabled: PropTypes.bool.isRequired,
 };
 
 FormModal.defaultProps = {
