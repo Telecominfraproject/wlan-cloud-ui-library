059cae58f0e562f22ec0b5d28f56166ac9277f8b
Sean Macfarlane <sean.g.macfarlane@gmail.com>
2021-03-27 17:52:00 -0400

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,177 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack/webpack.common');
+/* eslint-disable import/no-extraneous-dependencies */
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const path = require('path');
 
-const envs = {
-  development: 'dev',
-  production: 'prod',
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: ['babel-polyfill', './src/index.js'],
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: `[name].[chunkhash].js`,
+    chunkFilename: `[name].[chunkhash].js`,
+    libraryTarget: 'umd',
+    library: {
+      name: '@tip-wlan/wlan-cloud-ui-library',
+      type: 'umd',
+    },
+    umdNamedDefine: true,
+  },
+
+  externals: [
+    {
+      react: {
+        amd: 'react',
+        commonjs: 'react',
+        commonjs2: 'react',
+        root: 'React',
+      },
+      lodash: {
+        commonjs: 'lodash',
+        commonjs2: 'lodash',
+        amd: 'lodash',
+        root: '_', // indicates global variable
+      },
+      'babel-runtime': {
+        commonjs: 'babel-runtime',
+        commonjs2: 'babel-runtime',
+      },
+      'babel-polyfill': {
+        commonjs: 'babel-polyfill',
+        commonjs2: 'babel-polyfill',
+      },
+      moment: {
+        root: 'moment',
+        commonjs2: 'moment',
+        commonjs: 'moment',
+        amd: 'moment',
+      },
+    },
+    /^highcharts\/.+$/,
+    'react-dom',
+    'antd',
+    '@ant-design/icons',
+    '@babel/core',
+    'highcharts-react-official',
+    'prop-types',
+    'react-router-dom',
+    'react-jsx-highcharts',
+    'react-jsx-highstock',
+    'uuid',
+  ],
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          warnings: false,
+          compress: {
+            comparisons: false,
+          },
+          parse: {},
+          mangle: true,
+          output: {
+            comments: false,
+            ascii_only: true,
+          },
+        },
+        parallel: true,
+      }),
+      new CssMinimizerPlugin(),
+    ],
+    nodeEnv: 'production',
+    sideEffects: true,
+    concatenateModules: true,
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: 10,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: false,
+              localsConvention: 'camelCase',
+              modules: {
+                localIdentName: '[local]___[hash:base64:5]',
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'less-loader', // compiles Less to CSS
+            options: {
+              lessOptions: {
+                javascriptEnabled: true,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  resolve: {
+    modules: ['node_modules', 'src'],
+    alias: {
+      app: path.resolve(__dirname, '../', 'src'),
+    },
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+
+    new MiniCssExtractPlugin({
+      filename: `css/[name].css`,
+      chunkFilename: `css/[hash].css`,
+    }),
+
+    new CompressionPlugin({
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+
+    // new BundleAnalyzerPlugin(),
+  ],
 };
-
-/* eslint-disable global-require,import/no-dynamic-require */
-const env = envs[process.env.NODE_ENV || 'production'];
-const envConfig = require(`./webpack/webpack.${env}.js`);
-module.exports = merge(common, envConfig);
