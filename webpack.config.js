1fed5e5518118f4b937ae8641d007ae0f8698501
Sean Macfarlane <sean.g.macfarlane@gmail.com>
2021-03-29 01:14:52 -0400

    fixed webpacl

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,17 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV,
-  entry: ['babel-polyfill', './src/index.js'],
+  entry: './src/index.js',
 
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: `[name].[chunkhash].js`,
-    chunkFilename: `[name].[chunkhash].js`,
+    publicPath: '/dist/',
+    filename: `index.js`,
     libraryTarget: 'umd',
     library: {
       name: '@tip-wlan/wlan-cloud-ui-library',
@@ -23,6 +19,8 @@ module.exports = {
     },
     umdNamedDefine: true,
   },
+
+  target: 'node', // ingore all dependencies
 
   externals: [
     {
@@ -65,46 +63,6 @@ module.exports = {
     'react-jsx-highstock',
     'uuid',
   ],
-
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          warnings: false,
-          compress: {
-            comparisons: false,
-          },
-          parse: {},
-          mangle: true,
-          output: {
-            comments: false,
-            ascii_only: true,
-          },
-        },
-        parallel: true,
-      }),
-      new CssMinimizerPlugin(),
-    ],
-    nodeEnv: 'production',
-    sideEffects: true,
-    concatenateModules: true,
-    runtimeChunk: 'single',
-    splitChunks: {
-      chunks: 'all',
-      maxInitialRequests: 10,
-      minSize: 0,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-            return `npm.${packageName.replace('@', '')}`;
-          },
-        },
-      },
-    },
-  },
 
   module: {
     rules: [
@@ -163,13 +121,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: `css/[name].css`,
       chunkFilename: `css/[hash].css`,
-    }),
-
-    new CompressionPlugin({
-      algorithm: 'gzip',
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8,
     }),
 
     // new BundleAnalyzerPlugin(),
