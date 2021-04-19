46b2a975cb00f3cc582b2fc192f663a328246553
irtiza-h30 <ihasa074@uottawa.ca>
2021-04-19 17:05:54 -0400

    NAS configuration fields for SSID profile

--- src/containers/ProfileDetails/components/SSID/index.js
+++ src/containers/ProfileDetails/components/SSID/index.js
@@ -7,7 +7,7 @@ import ThemeContext from 'contexts/ThemeContext';
 import globalStyles from 'styles/index.scss';
 import styles from '../index.module.scss';
 import { defaultSsidProfile } from '../constants';
-import { RADIOS, ROAMING, PROFILES } from '../../constants/index';
+import { RADIOS, ROAMING, PROFILES, IP_REGEX } from '../../constants/index';
 
 const { Item } = Form;
 const { Option } = Select;
@@ -78,6 +78,20 @@ const SSIDForm = ({
         details?.radiusAcountingServiceInterval ||
         defaultSsidProfile.radiusAcountingServiceInterval,
       dynamicVlan: details?.dynamicVlan || defaultSsidProfile.dynamicVlan,
+      radiusClientConfiguration: {
+        nasClientId:
+          details?.radiusClientConfiguration?.nasClientId ??
+          defaultSsidProfile.radiusClientConfiguration.nasClientId,
+        nasClientIp:
+          details?.radiusClientConfiguration?.nasClientIp ??
+          defaultSsidProfile.radiusClientConfiguration.nasClientIp,
+        userDefinedNasId:
+          details?.radiusClientConfiguration?.userDefinedNasId ??
+          defaultSsidProfile.radiusClientConfiguration.userDefinedNasId,
+        userDefinedNasIp:
+          details?.radiusClientConfiguration?.userDefinedNasIp ??
+          defaultSsidProfile.radiusClientConfiguration.userDefinedNasIp,
+      },
     });
   }, [form, details]);
 
@@ -108,7 +122,7 @@ const SSIDForm = ({
             },
           ]}
         >
-          <Input className={globalStyles.field} name="ssidName" placeholder="Enter SSID name" />
+          <Input className={globalStyles.field} placeholder="Enter SSID name" />
         </Item>
 
         <Item
@@ -356,74 +370,6 @@ const SSIDForm = ({
             </span>
           </Item>
         )}
-        {(mode === 'wpaRadius' ||
-          mode === 'wpa2Radius' ||
-          mode === 'wpa2OnlyRadius' ||
-          mode === 'wpa3OnlyEAP' ||
-          mode === 'wpa3MixedEAP') && (
-          <>
-            <Item
-              name="radiusServiceId"
-              label="RADIUS Profile"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please select a RADIUS profile',
-                },
-              ]}
-            >
-              <Select
-                className={globalStyles.field}
-                placeholder="Select RADIUS Profile"
-                onPopupScroll={e => onFetchMoreProfiles(e, PROFILES.radius)}
-                showSearch={onSearchProfile}
-                filterOption={false}
-                onSearch={name => onSearchProfile(name, PROFILES.radius)}
-                loading={loadingRadiusProfiles}
-                notFoundContent={!loadingRadiusProfiles && <Empty />}
-                labelInValue
-              >
-                {radiusProfiles.map(profile => (
-                  <Option key={profile.id} value={profile.id}>
-                    {profile.name}
-                  </Option>
-                ))}
-              </Select>
-            </Item>
-            <Item
-              name="radiusAcountingServiceInterval"
-              label="RADIUS Accounting Interval"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please enter a RADIUS accounting interval',
-                },
-                () => ({
-                  validator(_rule, value) {
-                    if (!value || (value >= 60 && value <= 600) || value === '0') {
-                      return Promise.resolve();
-                    }
-                    return Promise.reject(new Error('0 or 60 - 600'));
-                  },
-                }),
-              ]}
-            >
-              <Input
-                className={globalStyles.field}
-                placeholder="0 or 60 - 600 "
-                type="number"
-                min={60}
-                max={600}
-                addonAfter={
-                  <Tooltip
-                    title="Interval can be 0 or a number between 60 and 600"
-                    text="Seconds"
-                  />
-                }
-              />
-            </Item>
-          </>
-        )}
 
         {(mode === 'wpaPSK' ||
           mode === 'wpa2PSK' ||
@@ -601,6 +547,177 @@ const SSIDForm = ({
                   <span className={styles.Disclaimer}>Disabled</span>
                 )}
               </Item>
+            );
+          }}
+        </Item>
+      </Card>
+      <Card title="RADIUS">
+        {(mode === 'wpaRadius' ||
+          mode === 'wpa2Radius' ||
+          mode === 'wpa2OnlyRadius' ||
+          mode === 'wpa3OnlyEAP' ||
+          mode === 'wpa3MixedEAP') && (
+          <>
+            <Item
+              name="radiusServiceId"
+              label="RADIUS Profile"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please select a RADIUS profile',
+                },
+              ]}
+            >
+              <Select
+                className={globalStyles.field}
+                placeholder="Select RADIUS Profile"
+                onPopupScroll={e => onFetchMoreProfiles(e, PROFILES.radius)}
+                showSearch={onSearchProfile}
+                filterOption={false}
+                onSearch={name => onSearchProfile(name, PROFILES.radius)}
+                loading={loadingRadiusProfiles}
+                notFoundContent={!loadingRadiusProfiles && <Empty />}
+                labelInValue
+              >
+                {radiusProfiles.map(profile => (
+                  <Option key={profile.id} value={profile.id}>
+                    {profile.name}
+                  </Option>
+                ))}
+              </Select>
+            </Item>
+            <Item
+              name="radiusAcountingServiceInterval"
+              label="RADIUS Accounting Interval"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please enter a RADIUS accounting interval',
+                },
+                () => ({
+                  validator(_rule, value) {
+                    if (!value || (value >= 60 && value <= 600) || value === '0') {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(new Error('0 or 60 - 600'));
+                  },
+                }),
+              ]}
+            >
+              <Input
+                className={globalStyles.field}
+                placeholder="0 or 60 - 600 "
+                type="number"
+                min={60}
+                max={600}
+                addonAfter={
+                  <Tooltip
+                    title="Interval can be 0 or a number between 60 and 600"
+                    text="Seconds"
+                  />
+                }
+              />
+            </Item>
+          </>
+        )}
+        <Item
+          label="NAS ID"
+          name={['radiusClientConfiguration', 'nasClientId']}
+          rules={[
+            {
+              required: true,
+              message: 'Please select NAS ID',
+            },
+          ]}
+        >
+          <Select
+            data-testid="securityMode"
+            className={globalStyles.field}
+            placeholder="Select NAS ID"
+          >
+            <Option value="BSSID">BSSID</Option>
+            <Option value="AP_BASE_MAC">AP Base MAC Address</Option>
+            <Option value="USER_DEFINED">Manual</Option>
+          </Select>
+        </Item>
+        <Item
+          noStyle
+          shouldUpdate={(prevValues, currentValues) =>
+            prevValues.radiusClientConfiguration?.nasClientId !==
+            currentValues.radiusClientConfiguration?.nasClientId
+          }
+        >
+          {({ getFieldValue }) => {
+            return (
+              getFieldValue(['radiusClientConfiguration', 'nasClientId']) === 'USER_DEFINED' && (
+                <Item
+                  wrapperCol={{ offset: 5, span: 15 }}
+                  name={['radiusClientConfiguration', 'userDefinedNasId']}
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please enter NAS ID',
+                    },
+                    {
+                      pattern: /^\s*(?:\S\s*){3,}$/,
+                      message: 'NAS-ID must be atleast 3 characters',
+                    },
+                  ]}
+                >
+                  <Input className={globalStyles.field} placeholder="Enter NAS ID" />
+                </Item>
+              )
+            );
+          }}
+        </Item>
+        <Item
+          label="NAS IP"
+          name={['radiusClientConfiguration', 'nasClientIp']}
+          rules={[
+            {
+              required: true,
+              message: 'Please select NAS IP',
+            },
+          ]}
+        >
+          <Select
+            data-testid="securityMode"
+            className={globalStyles.field}
+            placeholder="Select NAS IP"
+          >
+            <Option value="WAN_IP">WAN</Option>
+            <Option value="PROXY_IP">Proxy</Option>
+            <Option value="USER_DEFINED">Manual</Option>
+          </Select>
+        </Item>
+        <Item
+          noStyle
+          shouldUpdate={(prevValues, currentValues) =>
+            prevValues.radiusClientConfiguration?.nasClientIp !==
+            currentValues.radiusClientConfiguration?.nasClientIp
+          }
+        >
+          {({ getFieldValue }) => {
+            return (
+              getFieldValue(['radiusClientConfiguration', 'nasClientIp']) === 'USER_DEFINED' && (
+                <Item
+                  wrapperCol={{ offset: 5, span: 15 }}
+                  name={['radiusClientConfiguration', 'userDefinedNasIp']}
+                  hasFeedback
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please enter NAS IP',
+                    },
+                    {
+                      pattern: IP_REGEX,
+                      message: 'Enter in the format [0-255].[0-255].[0-255].[0-255]',
+                    },
+                  ]}
+                >
+                  <Input className={globalStyles.field} placeholder="Enter NAS IP" />
+                </Item>
+              )
             );
           }}
         </Item>
