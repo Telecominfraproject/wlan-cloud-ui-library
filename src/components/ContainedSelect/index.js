6997dd6a1b5d090f80a33014251f5fff9b7c6794
ToanDo <Toan.Do@netexperience.com>
2021-04-21 17:11:24 -0400

    removed unneeded prop

--- src/components/ContainedSelect/index.js
+++ src/components/ContainedSelect/index.js
@@ -2,24 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Select as AntdSelect } from 'antd';
 
-const ContainedSelect = ({ children, className, ...restProps }) => (
-  <AntdSelect
-    className={className}
-    getPopupContainer={triggerNode => triggerNode.parentElement}
-    {...restProps}
-  >
+const ContainedSelect = ({ children, ...restProps }) => (
+  <AntdSelect getPopupContainer={triggerNode => triggerNode.parentElement} {...restProps}>
     {children}
   </AntdSelect>
 );
 
 ContainedSelect.propTypes = {
   children: PropTypes.node,
-  className: PropTypes.string,
 };
 
 ContainedSelect.defaultProps = {
   children: null,
-  className: '',
 };
 
 export default ContainedSelect;
